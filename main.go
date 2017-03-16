616645a765ccc01bde91f25f96fe5886ddcb2a2a
Max Siegieda <maxsiegieda@gmail.com>
2017-03-16 14:39:17 +0000

    create core types for our domain

--- main.go
+++ main.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+type Account struct {
+  gorm.Model
+  GithubID string
+  Email string
+}
+
+type Project struct {
+  gorm.Model
+  Name string
+}
+
+type AuthToken struct {
+  gorm.Model
+  Token string
+}
+
+type Build struct {
+  gorm.Model
+  InputArtifact string
+  OutputArtifact string
+  CreatedTime string
+  OutputStream string
+}
+
 func main() {
+
 	r := gin.Default()
 
 	// Ping test
