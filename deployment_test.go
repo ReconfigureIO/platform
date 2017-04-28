ce10f09c1cd5eb4ab1d13dba19de3cc604452c49
Max Siegieda <maxsiegieda@gmail.com>
2017-04-28 13:42:13 +0100

    set up gin within test file

--- deployment_test.go
+++ deployment_test.go
@@ -1,21 +1,56 @@
 package api
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/ReconfigureIO/platform/api"
+	"github.com/ReconfigureIO/platform/auth"
+	"github.com/ReconfigureIO/platform/migration"
+	"github.com/ReconfigureIO/platform/routes"
 )
 
 func TestPing(t *testing.T) {
 
-	setupDB()
-	defer clearDB()
-
-	ts := httptest.NewServer(Setup())
+	ts := httptest.NewServer(setupGin())
 
 	req, _ := http.NewRequest("GET", "/ping", nil)
 	resp := httptest.NewRecorder()
 	ts.ServeHTTP(resp, req)
 
 	assert.Equal(t, resp.Body.String(), "pong pong")
+}
+
+func setupDB() {
+
+}
+
+func setupGin() {
+
+	gin.SetMode(gin.TestMode)
+
+	r := gin.Default()
+
+	secretKey := os.Getenv("SECRET_KEY_BASE")
+
+	// setup components
+	db := setupDB()
+
+	store := sessions.NewCookieStore([]byte(secretKey))
+	r.Use(sessions.Sessions("paus", store))
+	r.Use(auth.SessionAuth(db))
+
+	r.LoadHTMLGlob("templates/*")
+
+	// ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong pong")
+	})
+
+	routes.SetupRoutes(r, db)
+
+	// Listen and Server in 0.0.0.0:$PORT
+	r.Run(":" + port)
 }
