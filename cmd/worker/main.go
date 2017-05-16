aed319a152cc9d82b38706109d2bfdf2a341db44
Josh Bohde <josh@joshbohde.com>
2017-05-16 12:15:19 -0500

    Add /health for workers

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -5,15 +5,33 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
+	gormConnDets := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open("postgres", gormConnDets)
+
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
 	port, found := os.LookupEnv("PORT")
 	if !found {
 		port = "8080"
 	}
 
 	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		err := db.DB().Ping()
+		if err != nil {
+			c.String(500, "error connecting to db")
+		} else {
+			c.String(200, "OK")
+		}
+	})
+
 	r.POST("/hello", func(c *gin.Context) {
 		log.Printf("Hello world\n")
 		c.String(200, "hello")
