965328ed61e5395a4536ce06ed4b2487e45b883c
Josh Bohde <josh@joshbohde.com>
2017-06-06 16:07:07 -0500

    break out an admin package.

--- handlers/admin/invites.go
+++ handlers/admin/invites.go
@@ -1,4 +1,4 @@
-package auth
+package admin
 
 import (
 	"github.com/ReconfigureIO/platform/models"
--- routes/admin.go
+++ routes/admin.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	"github.com/ReconfigureIO/platform/handlers/auth"
+	"github.com/ReconfigureIO/platform/handlers/admin"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
 // SetupAdmin sets up admin routes.
 func SetupAdmin(r gin.IRouter, db *gorm.DB) {
-	admin := auth.InviteAdmin{db}
+	admin := admin.InviteAdmin{db}
 	invites := r.Group("/invites")
 	{
 		invites.POST("", admin.Create)
