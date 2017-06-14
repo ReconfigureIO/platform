9dbed4a8cde0a03835b270140b44dc744f6037c9
Abiola Ibrahim <abiola89@gmail.com>
2017-06-14 19:01:11 +0100

    fix test name

--- handlers/api/billing_test.go
+++ handlers/api/billing_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TimeToSQLStr(t *testing.T) {
+func TestTimeToSQLStr(t *testing.T) {
 	utcTime := time.Date(2010, 2, 11, 3, 20, 30, 0, time.UTC)
 	expected := "2010-02-01 00:00:00"
 	if ms := timeToSQLStr(monthStart(utcTime)); ms != expected {
