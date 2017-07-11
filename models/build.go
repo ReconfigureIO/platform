3f8551083b648c35c9012989a19498c953a84c0a
Max Siegieda <maxsiegieda@gmail.com>
2017-07-11 14:57:42 +0100

    add function to look up builds with a status

---
+++ models/build.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type BuildRepo interface {
+	// Return a list of deployments, with the statuses specified,
+	// limited to that number
+	GetBuildsWithStatus([]string, int) ([]Build, error)
+}
+
+type PostgresRepo struct {
+	DB *gorm.DB
+}
+
+const (
+	SQL_BUILD_STATUS = `SELECT j.id
+FROM builds j
+LEFT join batch_job_events e
+ON j.batch_job_id = e.batch_job_id
+    AND e.timestamp = (
+        SELECT max(timestamp)
+        FROM batch_job_events e1
+        WHERE j.batch_job_id = e1.batch_job_id
+    )
+WHERE (e.status in (?))
+LIMIT ?
+`
+)
+
+func (repo *PostgresRepo) GetBuildsWithStatus(statuses []string, limit int) ([]Build, error) {
+	db := repo.DB
+	rows, err := db.Raw(SQL_BUILD_STATUS, statuses, limit).Rows()
+	if err != nil {
+		return nil, err
+	}
+
+	ids := []string{}
+	for rows.Next() {
+		var id string
+		rows.Scan(&id)
+		ids = append(ids, id)
+	}
+	rows.Close()
+
+	var builds []Build
+	err = db.Preload("BatchJob").Where("id in (?)", ids).Find(&builds).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return builds, nil
+}
