b8d856a874cbe933d1bced7418a6b0c1a86587dc
Max Siegieda <maxsiegieda@gmail.com>
2017-09-12 13:06:23 +0100

    cut intercom stuff out of events

--- service/events/events.go
+++ service/events/events.go
@@ -1,49 +1,21 @@
 package events
 
 import (
-	// "bytes"
-	// "context"
-	// "errors"
-	// "io"
-	// "io/ioutil"
-	// "os"
 	"time"
-	// "github.com/ReconfigureIO/platform/models"
-	// "github.com/abiosoft/errs"
+	
+	"github.com/ReconfigureIO/platform/service/intercom"
 )
 
-// Service is an AWS service.
-type Service interface {
-	QueueEvent()
-	PostEvents()
-	Conf() *ServiceConfig
+var eventQueue []Event
+
+func QueueEvent(event Event) error {
+	eventQueue = append(eventQueue, event)
 }
 
-type service struct {
-	conf ServiceConfig
-}
-
-// ServiceConfig holds configuration for service.
-type ServiceConfig struct {
-	//TODO
-}
-
-// New creates a new service with conf.
-func New(conf ServiceConfig) Service {
-	s := service{conf: conf}
-	return &s
-}
-
-func (s *service) QueueEvent() (string, error) {
-	//TODO
-}
-
-func (s *service) PostEvents() error {
-	//TODO
-}
-
-func (s *service) Conf() *ServiceConfig {
-	return &s.conf
+func PostEvents() error {
+	for e := range eventQueue {
+		intercom.Save(e)
+	}
 }
 
 type Event struct {
