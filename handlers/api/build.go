12a06d02a819e93db5f58d052ef3b3aa1ef340b4
Max Siegieda <maxsiegieda@gmail.com>
2017-09-14 10:17:55 +0100

    HTTP 200 with error message for missing sub-resource

--- handlers/api/build.go
+++ handlers/api/build.go
@@ -90,8 +90,7 @@ func (b Build) Report(c *gin.Context) {
 	report, err := buildRepo.GetBuildReport(build)
 
 	if err != nil {
-		c.Error(err)
-		sugar.ErrResponse(c, 500, nil)
+		sugar.SuccessResponse(c, 200, err)
 		return
 	}
 
