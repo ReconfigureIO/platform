0cfc79ec1002baa5cdb560d1b3f59f3dec21e08f
Josh Bohde <josh@joshbohde.com>
2017-09-21 10:57:37 -0500

    Don't create duplicate subscriptions in Stripe
    With the addition of trial periods, a subscription can be Trialing,
    which is not the active we were checking for.

---
+++ models/subscription_test.go
@@ -0,0 +1,61 @@
+// +build integration
+
+package models
+
+import (
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	subscriptions "github.com/stripe/stripe-go/sub"
+)
+
+func TestShouldNotCreateDuplicateSubscriptions(t *testing.T) {
+	RunTransaction(func(db *gorm.DB) {
+		u := User{
+			ID:         "josh",
+			Email:      "josh@joshbohde.com",
+			GithubName: "joshbohde",
+			// important part
+			StripeToken: "cus_AgZQTeZbnY6AE4",
+		}
+
+		subs := repo(db)
+		c, err := subs.cachedCustomer(u)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Cancel all subscriptions
+		for _, val := range c.Subs.Values {
+			_, err = subscriptions.Cancel(val.ID, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		subs = repo(db)
+
+		_, err = subs.UpdatePlan(u, PlanSingleUser)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		subs = repo(db)
+
+		_, err = subs.UpdatePlan(u, PlanSingleUser)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		subs = repo(db)
+		c, err = subs.cachedCustomer(u)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(c.Subs.Values) != 1 {
+			t.Errorf("Expected 1 Subscription, but got %+v", c.Subs)
+		}
+
+	})
+}
