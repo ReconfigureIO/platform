bd5458ab14007b08349f2495b7246790321453e1
Josh Bohde <josh@joshbohde.com>
2017-09-21 13:32:37 -0500

    oh, we need that db.

--- models/subscription_test.go
+++ models/subscription_test.go
@@ -5,54 +5,57 @@ package models
 import (
 	"testing"
 
+	"github.com/jinzhu/gorm"
 	subscriptions "github.com/stripe/stripe-go/sub"
 )
 
 func TestShouldNotCreateDuplicateSubscriptions(t *testing.T) {
-	u := User{
-		ID:         "josh",
-		Email:      "josh@joshbohde.com",
-		GithubName: "joshbohde",
-		// important part
-		StripeToken: "cus_AgZQTeZbnY6AE4",
-	}
+	RunTransaction(func(db *gorm.DB) {
+		u := User{
+			ID:         "josh",
+			Email:      "josh@joshbohde.com",
+			GithubName: "joshbohde",
+			// important part
+			StripeToken: "cus_AgZQTeZbnY6AE4",
+		}
 
-	subs := repo(db)
-	c, err := subs.cachedCustomer(u)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Cancel all subscriptions
-	for _, val := range c.Subs.Values {
-		_, err = subscriptions.Cancel(val.ID, nil)
+		subs := repo(db)
+		c, err := subs.cachedCustomer(u)
 		if err != nil {
 			t.Fatal(err)
 		}
-	}
 
-	subs = repo(db)
+		// Cancel all subscriptions
+		for _, val := range c.Subs.Values {
+			_, err = subscriptions.Cancel(val.ID, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
 
-	_, err = subs.UpdatePlan(u, PlanSingleUser)
-	if err != nil {
-		t.Fatal(err)
-	}
+		subs = repo(db)
 
-	subs = repo(db)
+		_, err = subs.UpdatePlan(u, PlanSingleUser)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	_, err = subs.UpdatePlan(u, PlanSingleUser)
-	if err != nil {
-		t.Fatal(err)
-	}
+		subs = repo(db)
 
-	subs = repo(db)
-	c, err = subs.cachedCustomer(u)
-	if err != nil {
-		t.Fatal(err)
-	}
+		_, err = subs.UpdatePlan(u, PlanSingleUser)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(c.Subs.Values) != 1 {
-		t.Errorf("Expected 1 Subscription, but got %+v", c.Subs)
-	}
+		subs = repo(db)
+		c, err = subs.cachedCustomer(u)
+		if err != nil {
+			t.Fatal(err)
+		}
 
+		if len(c.Subs.Values) != 1 {
+			t.Errorf("Expected 1 Subscription, but got %+v", c.Subs)
+		}
+
+	})
 }
