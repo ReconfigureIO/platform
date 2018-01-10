efa054b2dd68d1d05052852a9a3f023b6ad28f11
Max Siegieda <maxsiegieda@gmail.com>
2018-01-10 12:06:08 +0000

    make distinction between API and Cron in logs

--- config/logging.go
+++ config/logging.go
@@ -9,6 +9,7 @@ func SetupLogging(version string, conf *Config) error {
 	ctx := logrus.Fields{
 		"Environment": conf.Reco.Env,
 		"Version":     version,
+		"Application": conf.ProgramName,
 	}
 	hook, err := logruzio.New(conf.Reco.LogzioToken, conf.ProgramName, ctx)
 	if err != nil {
