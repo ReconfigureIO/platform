f1fb88c8914a8606c399aebb3c6af928a30cbec4
Max Siegieda <maxsiegieda@gmail.com>
2018-02-13 09:41:55 +0000

    allow https connections from ayup's staging reconfigure dashboard

--- main.go
+++ main.go
@@ -96,6 +96,7 @@ func main() {
 			"http://local.reconfigure.io",
 			"http://local.reconfigure.io:4200",
 			"https://reconfigure.io",
+			"https://reconfigure-app.ayup.io",
 		}
 	default:
 		corsConfig.AllowOrigins = []string{
