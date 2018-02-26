7f28e456df1f31d047af32a9775432e8d2660ce4
Max Siegieda <maxsiegieda@gmail.com>
2018-02-26 11:52:41 +0000

    add test for cw_id_watcher

--- service/cw_id_watcher/cw_id_watcher_test.go
+++ service/cw_id_watcher/cw_id_watcher_test.go
@@ -1,95 +1,34 @@
-package afi_watcher
+package cw_id_watcher
 
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/ReconfigureIO/platform/models"
 	"github.com/ReconfigureIO/platform/service/aws"
 	"github.com/golang/mock/gomock"
 )
 
-func TestFindAFI(t *testing.T) {
+func TestFindLogNames(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	d := models.NewMockBuildRepo(mockCtrl)
 	b := models.NewMockBatchRepo(mockCtrl)
-	mockService := aws.NewMockService(mockCtrl)
+	a := aws.NewMockService(mockCtrl)
 
-	watcher := NewAFIWatcher(d, mockService, b)
+	watcher := NewLogWatcher(a, b)
 
-	// the time.Now we return as part of afistatus comes back as
-	// part of the call to AddEvent
-	timeNow := time.Now()
-	afistatus := map[string]aws.Status{"agfi-foobar": aws.Status{"available", timeNow}}
+	batchJobIDs := []string{"foobar"}
+	cwLogNames := map[string]string{"foobar": "cwLogName"}
 
-	build := models.Build{
-		FPGAImage: "agfi-foobar",
-		BatchJob: models.BatchJob{
-			Events: []models.BatchJobEvent{
-				models.BatchJobEvent{
-					Timestamp: timeNow,
-					Status:    models.StatusCreatingImage,
-					Message:   "afi-foobar",
-				},
-			},
-		},
-	}
-	builds := []models.Build{build}
 	ctx := context.Background()
 	limit := 100
 
-	event := &models.BatchJobEvent{
-		Timestamp: timeNow,
-		Status:    models.StatusCompleted,
-		Message:   "Image generation succeeded",
-		Code:      0,
-	}
+	a.EXPECT().ListBatchJobs(ctx, limit).Return(batchJobIDs, nil)
+	a.EXPECT().GetCwLogNames(ctx, batchJobIDs).Return(cwLogNames, nil)
+	b.EXPECT().SetCwLogName("foobar", "cwLogName").Return(nil)
 
-	d.EXPECT().GetBuildsWithStatus(creating_statuses, limit).Return(builds, nil)
-	mockService.EXPECT().DescribeAFIStatus(ctx, builds).Return(afistatus, nil)
-	b.EXPECT().AddEvent(build.BatchJob, *event).Return(nil)
-
-	err := watcher.FindAFI(ctx, limit)
-
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestFindAFISkipsInvalidStatus(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	d := models.NewMockBuildRepo(mockCtrl)
-	b := models.NewMockBatchRepo(mockCtrl)
-	mockService := aws.NewMockService(mockCtrl)
-
-	watcher := NewAFIWatcher(d, mockService, b)
-
-	afistatus := map[string]aws.Status{"agfi-foobar": aws.Status{"invalid-status", time.Now()}}
-
-	build := models.Build{
-		FPGAImage: "agfi-foobar",
-		BatchJob: models.BatchJob{
-			Events: []models.BatchJobEvent{
-				models.BatchJobEvent{
-					Status:  "CREATING_IMAGE",
-					Message: "afi-foobar",
-				},
-			},
-		},
-	}
-	builds := []models.Build{build}
-	ctx := context.Background()
-	limit := 100
-
-	d.EXPECT().GetBuildsWithStatus(creating_statuses, limit).Return(builds, nil)
-	mockService.EXPECT().DescribeAFIStatus(ctx, builds).Return(afistatus, nil)
-
-	err := watcher.FindAFI(ctx, limit)
+	err := watcher.FindLogNames(ctx, limit)
 
 	if err != nil {
 		t.Error(err)
