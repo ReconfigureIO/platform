7d341bfd8432b55547cd67796a351d94c1dcf9ff
Peter Waller <p@pwaller.net>
2018-05-29 10:56:24 +0100

    Move {service/github => models}.CreateOrUpdateUser

--- service/auth/github/github.go
+++ service/auth/github/github.go
@@ -73,30 +73,7 @@ func (s *Service) GetOrCreateUser(ctx context.Context, accessToken string, creat
 		return u, UserError("No valid email found")
 	}
 
-	u, err = createOrUpdateUser(s.db, u, createNew)
+	u, err = models.CreateOrUpdateUser(s.db, u, createNew)
 
-	return u, err
-}
-
-func createOrUpdateUser(db *gorm.DB, u models.User, createNew bool) (models.User, error) {
-	q := db.Where(models.User{GithubID: u.GithubID})
-
-	var user models.User
-	if err := q.First(&user).Error; err != nil {
-		// not found
-		user = models.NewUser()
-		if err != gorm.ErrRecordNotFound {
-			return user, err
-		}
-		if !createNew {
-			return user, err
-		}
-	}
-
-	err := q.Attrs(user).FirstOrInit(&u).Error
-	if err != nil {
-		return u, err
-	}
-	db.Save(&u)
 	return u, err
 }
