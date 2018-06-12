cb24d5282906ff33ad22935f23d5f5b25a43ca80
Peter Waller <p@pwaller.net>
2018-06-12 14:07:09 +0100

    Rename s/Schedule/Enqueue/

--- cmd/fake-batch/semaphore.go
+++ cmd/fake-batch/semaphore.go
@@ -15,11 +15,11 @@ type jobQueueSemaphores struct {
 	// a semaphore per channel.
 	running map[Q]chan struct{}
 
-	// scheduleCalls is used internally to process scheduling events
-	scheduleCalls chan scheduleCall
+	// enqueueCalls is used internally to process enqueue() calls.
+	enqueueCalls chan enqueueCall
 }
 
-type scheduleCall struct {
+type enqueueCall struct {
 	q   Q
 	run func()
 }
@@ -29,9 +29,9 @@ type scheduleCall struct {
 // construction, 1 job is Max in flight for that Q.
 func newJobQueueSemaphores(nMaxInFlight map[Q]int) jobQueueSemaphores {
 	s := jobQueueSemaphores{
-		nMaxInFlight:  nMaxInFlight,
-		running:       map[Q]chan struct{}{},
-		scheduleCalls: make(chan scheduleCall, 16),
+		nMaxInFlight: nMaxInFlight,
+		running:      map[Q]chan struct{}{},
+		enqueueCalls: make(chan enqueueCall, 16),
 	}
 
 	go s.run()
@@ -40,12 +40,12 @@ func newJobQueueSemaphores(nMaxInFlight map[Q]int) jobQueueSemaphores {
 }
 
 func (s *jobQueueSemaphores) run() {
-	for c := range s.scheduleCalls {
-		s.schedule(c.q, c.run)
+	for c := range s.enqueueCalls {
+		s.enqueue(c.q, c.run)
 	}
 }
 
-func (s *jobQueueSemaphores) schedule(q Q, run func()) {
+func (s *jobQueueSemaphores) enqueue(q Q, run func()) {
 	runningChan, ok := s.running[q]
 	if !ok {
 		s.initQueue(q)
@@ -72,8 +72,9 @@ func (s *jobQueueSemaphores) qToNMaxInFlight(q Q) int {
 	return defaultNMaxInFlight
 }
 
-// Schedule calls run() when a slot becomes free in the given q.
+// Enqueue calls run() when a slot becomes free in the given q.
 // run() must block until the resources it uses are free.
-func (s *jobQueueSemaphores) Schedule(q Q, run func()) {
-	s.scheduleCalls <- scheduleCall{q, run}
+// Enqueue does not normally block.
+func (s *jobQueueSemaphores) Enqueue(q Q, run func()) {
+	s.enqueueCalls <- enqueueCall{q, run}
 }
