eb2bf87b0a688bf02125a1298e78c6b58c184e86
Max Siegieda <max.siegieda@reconfigure.io>
2018-09-07 12:14:10 +0100

    add basic fake implementation of sdaccel-builder for testing

---
+++ fake-sdaccel-builder/aws/simulate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+function post_event {
+    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "'"$1"'", "message": "'"$2"'", "code": '${3-0}'}' "$CALLBACK_URL" &> /dev/null
+}
+
+post_event STARTED
+
+echo "downloading source code... done"
+
+echo "running simulation..."
+
+for i in $(seq 1 100)
+    do echo $i%
+    sleep 1
+done
+
+post_event COMPLETED
