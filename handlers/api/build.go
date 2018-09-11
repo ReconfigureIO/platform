d718f7e5ffff0f57cdc293d63670dcfacdbb8987
Max Siegieda <maxsiegieda@gmail.com>
2018-09-11 12:40:38 +0100

    remove debug messages

--- handlers/api/build.go
+++ handlers/api/build.go
@@ -329,7 +329,6 @@ func (b Build) DownloadArtifact(c *gin.Context) {
 	if err != nil {
 		return
 	}
-	fmt.Println("I got this far boss")
 
 	if build.Status() != "COMPLETED" {
 		sugar.ErrResponse(c, 400, fmt.Sprintf("Build is '%s', not COMPLETED", build.Status()))
